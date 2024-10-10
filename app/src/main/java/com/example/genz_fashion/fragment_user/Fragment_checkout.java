a7c48496aadec9310f715e8714bf2d23ae9ff453
duccquagg137 <quangnd1307@gmail.com>
2024-10-10 16:26:52 +0700

    fragmentuser

--- app/src/main/java/com/example/genz_fashion/fragment_user/Fragment_checkout.java
+++ app/src/main/java/com/example/genz_fashion/fragment_user/Fragment_checkout.java
@@ -1,4 +1,4 @@
-package com.example.genz_fashion;
+package com.example.genz_fashion.fragment_user;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.genz_fashion.R;
 
 /**
  * A simple {@link Fragment} subclass.
