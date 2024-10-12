c7f128fc3233e92e3e13d5ec677dcc45f47e5c11
duccquagg137 <quangnd1307@gmail.com>
2024-10-12 16:03:03 +0700

    update

---
+++ app/src/main/java/com/example/genz_fashion/adapter/ViewPagerOderAdapter.java
@@ -0,0 +1,37 @@
+package com.example.genz_fashion.adapter;
+
+import android.support.annotation.NonNull;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.genz_fashion.fragmentUser.ActiveFragment;
+import com.example.genz_fashion.fragmentUser.CheckOutFragment;
+import com.example.genz_fashion.fragmentUser.CompletedFragment;
+
+public class ViewPagerOderAdapter extends FragmentStateAdapter {
+    public ViewPagerOderAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new ActiveFragment();
+            case 1:
+                return new CompletedFragment();
+            case 2:
+                return new CheckOutFragment();
+            default:
+                return new ActiveFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
--- app/src/main/java/com/example/genz_fashion/fragmentUser/MyOrderActivity.java
+++ app/src/main/java/com/example/genz_fashion/fragmentUser/MyOrderActivity.java
@@ -14,10 +14,38 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.genz_fashion.R;
+import com.example.genz_fashion.adapter.ViewPagerOderAdapter;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MyOrderActivity extends AppCompatActivity {
 
-  
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        ViewPagerOderAdapter adapter = new ViewPagerOderAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("Tab 1");
+                    break;
+                case 1:
+                    tab.setText("Tab 2");
+                    break;
+                case 2:
+                    tab.setText("Tab 3");
+                    break;
+            }
+        }).attach();
+    }
 }
