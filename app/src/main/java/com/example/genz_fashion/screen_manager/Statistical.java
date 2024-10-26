3e994f0401fb32b5d1dd3ae5573579f4d6f7aa0d
duccquagg137 <quangnd1307@gmail.com>
2024-10-26 15:51:33 +0700

    fix

--- app/src/main/java/com/example/genz_fashion/screen_manager/Statistical.java
+++ app/src/main/java/com/example/genz_fashion/screen_manager/Statistical.java
@@ -17,6 +17,5 @@ public class Statistical extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_statistical);
-
     }
 }
