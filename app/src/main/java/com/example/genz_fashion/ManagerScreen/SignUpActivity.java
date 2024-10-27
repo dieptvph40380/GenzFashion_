797a11fb4ce41dc617403b32fe62646f8449339e
duccquagg137 <quangnd1307@gmail.com>
2024-10-27 16:06:11 +0700

    commit

--- app/src/main/java/com/example/genz_fashion/ManagerScreen/SignUpActivity.java
+++ app/src/main/java/com/example/genz_fashion/ManagerScreen/SignUpActivity.java
@@ -1,22 +1,17 @@
 package com.example.genz_fashion.ManagerScreen;
 
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.genz_fashion.R;
+
+import com.example.genz_fashion.ManagerScreen.SignInActivity;
 import com.example.genz_fashion.UserScreen.MainActivity;
-import com.example.genz_fashion.UserScreen.OTPForgotPassActivity;
-import com.example.genz_fashion.UserScreen.SendOtpFragment;
 import com.example.genz_fashion.databinding.ActivitySignUpBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -24,114 +19,77 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class SignUpActivity extends AppCompatActivity {
 
-    private ActivitySignUpBinding binding;
+    FirebaseAuth mAuth;
+    ActivitySignUpBinding binding;
 
-    private String username, email, password, passwordConfirm;
-
-    public static final String KEY_USERNAME = "userName";
-    public static final String KEY_EMAIL = "email";
-    public static final String KEY_PASSWORD = "password";
-
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        mAuth = FirebaseAuth.getInstance();
         setContentView(binding.getRoot());
 
-        listener(); // Handle click events
-    }
-
-    private void listener() {
-        binding.btnBack.setOnClickListener(view -> {
-            // Finish the activity to go back
-            finish();
-        });
-
-        binding.edtPassword.addTextChangedListener(new TextWatcher() {
+        binding.edtNhaplaiPassword.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                String passwordInput = charSequence.toString();
-                if (passwordInput.length() >= 6) {
-                    Pattern pattern = Pattern.compile("[^a-zA-Z0-9]");
-                    Matcher matcher = pattern.matcher(passwordInput);
-                    boolean passwordsMatch = matcher.find();
-                    if (passwordsMatch) {
-                        binding.passwordTil.setHelperText("Your Password is Strong");
-                        binding.passwordTil.setHelperTextColor(ColorStateList.valueOf(getResources().getColor(R.color.green)));
-                        binding.passwordTil.setError("");
-                    } else {
-                        binding.passwordTil.setError("Please ensure your password contains at least one lowercase letter, one uppercase letter, and one special character.");
-                    }
-                } else {
-                    binding.passwordTil.setHelperText("Password must be 6 characters long");
-                    binding.passwordTil.setError("");
-                }
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+                startActivity(intent);
+                finish();
             }
-
+        });
+        binding.buttonDangKy.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void afterTextChanged(Editable editable) {}
+            public void onClick(View view) {
+                String email,password,name,repass;
+                email = String.valueOf(binding.edtEmail.getText());
+                password = String.valueOf(binding.edtPassword.getText());
+                name = String.valueOf(binding.edtUsername.getText());
+                repass = String.valueOf(binding.edtNhaplaiPassword.getText());
+                if (TextUtils.isEmpty(name)) {
+                    Toast.makeText(SignUpActivity.this, "Enter name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(SignUpActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(SignUpActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(repass)) {
+                    Toast.makeText(SignUpActivity.this, "Enter repassword", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                    Toast.makeText(SignUpActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+                                } else {
+
+                                    Toast.makeText(SignUpActivity.this, "failed.", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
         });
 
-        binding.buttonDangKy.setOnClickListener(view -> {
-            validateData();
-        });
-
-        binding.tvDangNhap.setOnClickListener(view -> {
-            // Navigate to the login activity
-            Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
-            startActivity(intent);
-        });
-    }
-
-    private void validateData() {
-        username = binding.edtUsername.getText().toString().trim();
-        email = binding.edtEmail.getText().toString().trim();
-        password = binding.edtPassword.getText().toString().trim();
-        passwordConfirm = binding.edtNhaplaiPassword.getText().toString().trim();
-
-        if (TextUtils.isEmpty(username)) {
-            binding.edtUsername.setError("Bạn cần nhập đủ thông tin");
-            binding.edtUsername.requestFocus();
-            return;
-        }
-
-        if (TextUtils.isEmpty(email)) {
-            binding.edtEmail.setError("Bạn cần nhập đủ thông tin");
-            binding.edtEmail.requestFocus();
-            return;
-        }
-
-        if (TextUtils.isEmpty(password)) {
-            binding.edtPassword.setError("Bạn cần nhập đủ thông tin");
-            binding.edtPassword.requestFocus();
-            return;
-        } else if (!password.equals(passwordConfirm)) {
-            binding.edtNhaplaiPassword.setError("Mật khẩu không khớp");
-            binding.edtNhaplaiPassword.requestFocus();
-            return;
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            binding.edtEmail.setError("Email không đúng định dạng");
-            binding.edtEmail.requestFocus();
-            return;
-        } else {
-            dangKy();
-        }
-    }
-
-    private void dangKy() {
-        // Pass data using Intent
-        Intent intent = new Intent(SignUpActivity.this, SendOtpFragment.class);
-        intent.putExtra(KEY_USERNAME, username);
-        intent.putExtra(KEY_EMAIL, email);
-        intent.putExtra(KEY_PASSWORD, password);
-        startActivity(intent);
     }
 }
