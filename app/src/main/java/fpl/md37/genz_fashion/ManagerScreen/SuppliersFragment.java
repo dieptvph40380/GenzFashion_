2dfe3cfae8151e1ad5ddca62154c16fa4941c42c
hnam56 <nandmemories56@gmail.com>
2024-11-01 14:20:30 +0700

    Get , Add, Delete Suppliers

--- app/src/main/java/fpl/md37/genz_fashion/ManagerScreen/SuppliersFragment.java
+++ app/src/main/java/fpl/md37/genz_fashion/ManagerScreen/SuppliersFragment.java
@@ -1,21 +1,19 @@
 package fpl.md37.genz_fashion.ManagerScreen;
 
+
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.genz_fashion.R;
 
 public class SuppliersFragment extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.fragment_suppliers);
+
     }
 }
