58effcd0251cafece59bebc90048a8691de9d857
duccquagg137 <quangnd1307@gmail.com>
2024-11-17 20:39:07 +0700

    fix

--- app/src/main/java/fpl/md37/genz_fashion/utils/FirebaseUtil.java
+++ app/src/main/java/fpl/md37/genz_fashion/utils/FirebaseUtil.java
@@ -26,10 +26,10 @@ public class FirebaseUtil {
     // Truy xuất thông tin người dùng hiện tại từ Firestore
     public static DocumentReference currentUserDetails() {
         if (!isLoggedIn()) {
-            throw new IllegalStateException("Client is not logged in.");
+            throw new IllegalStateException("User is not logged in.");
         }
         return FirebaseFirestore.getInstance()
-                .collection("Client")
+                .collection("users")
                 .document(currentUserId());
     }
 
@@ -45,9 +45,6 @@ public class FirebaseUtil {
                 .child(userId);
     }
 
-
-
-
     // Lấy StorageReference của ảnh đại diện người dùng khác
     public static StorageReference getOtherProfilePicStorageRef(String otherUserId) {
         if (otherUserId == null || otherUserId.isEmpty()) {
@@ -56,68 +53,6 @@ public class FirebaseUtil {
         return FirebaseStorage.getInstance()
                 .getReference()
                 .child("profile_pic")
-    public static String currentUserId(){
-        return FirebaseAuth.getInstance().getUid();
-    }
-
-    public static boolean isLoggedIn(){
-        if(currentUserId()!=null){
-            return true;
-        }
-        return false;
-    }
-
-    public static DocumentReference currentUserDetails(){
-        return FirebaseFirestore.getInstance().collection("users").document(currentUserId());
-    }
-
-    public static CollectionReference allUserCollectionReference(){
-        return FirebaseFirestore.getInstance().collection("users");
-    }
-
-    public static DocumentReference getChatroomReference(String chatroomId){
-        return FirebaseFirestore.getInstance().collection("chatrooms").document(chatroomId);
-    }
-
-    public static CollectionReference getChatroomMessageReference(String chatroomId){
-        return getChatroomReference(chatroomId).collection("chats");
-    }
-
-    public static String getChatroomId(String userId1,String userId2){
-        if(userId1.hashCode()<userId2.hashCode()){
-            return userId1+"_"+userId2;
-        }else{
-            return userId2+"_"+userId1;
-        }
-    }
-
-    public static CollectionReference allChatroomCollectionReference(){
-        return FirebaseFirestore.getInstance().collection("chatrooms");
-    }
-
-    public static DocumentReference getOtherUserFromChatroom(List<String> userIds){
-        if(userIds.get(0).equals(FirebaseUtil.currentUserId())){
-            return allUserCollectionReference().document(userIds.get(1));
-        }else{
-            return allUserCollectionReference().document(userIds.get(0));
-        }
-    }
-
-    public static String timestampToString(Timestamp timestamp){
-        return new SimpleDateFormat("HH:MM").format(timestamp.toDate());
-    }
-
-    public static void logout(){
-        FirebaseAuth.getInstance().signOut();
-    }
-
-    public static StorageReference getCurrentProfilePicStorageRef(){
-        return FirebaseStorage.getInstance().getReference().child("profile_pic")
-                .child(FirebaseUtil.currentUserId());
-    }
-
-    public static StorageReference  getOtherProfilePicStorageRef(String otherUserId){
-        return FirebaseStorage.getInstance().getReference().child("profile_pic")
                 .child(otherUserId);
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,19 @@
 pluginManagement {
     repositories {
-        google() // Kho chứa các plugin của Android
-        mavenCentral() // Kho Maven chính thống
-        gradlePluginPortal() // Kho chứa plugin Gradle
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS) // Ngăn chặn các kho cục bộ trong các mô-đun
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google() // Kho chính thức của Android
-        mavenCentral() // Kho Maven chính thống
-        maven("https://jitpack.io")
-    }
-}
-
-rootProject.name = "GenZ_Fashion" // Tên dự án
-include(":app") // Bao gồm mô-đun chính của ứng dụng
         google()
         mavenCentral()
         maven {
