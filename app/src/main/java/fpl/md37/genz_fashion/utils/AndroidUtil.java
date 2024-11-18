136f5accd9862856ec72e90906ca60314d0728a1
phamHoang04 <hoangzzz75@gmail.com>
2024-11-18 16:58:04 +0700

    fix loi sua thong tin

--- app/src/main/java/fpl/md37/genz_fashion/utils/AndroidUtil.java
+++ app/src/main/java/fpl/md37/genz_fashion/utils/AndroidUtil.java
@@ -1,7 +1,6 @@
 package fpl.md37.genz_fashion.utils;
 
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -10,15 +9,21 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 
 public class AndroidUtil {
-    public static void showToast(Context context,String message){
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+
+    // Hiển thị thông báo Toast
+    public static void showToast(Context context, String message) {
+        if (context != null && message != null) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        }
     }
 
-
-    public static void setProfilePic(Context context, Uri imageUri, ImageView imageView){
-        Glide.with(context).load(imageUri).apply(RequestOptions.circleCropTransform()).into(imageView);
+    // Cập nhật ảnh hồ sơ
+    public static void setProfilePic(Context context, Uri imageUri, ImageView imageView) {
+        if (context != null && imageUri != null && imageView != null) {
+            Glide.with(context)
+                    .load(imageUri)
+                    .apply(RequestOptions.circleCropTransform()) // Cắt hình tròn
+                    .into(imageView);
+        }
     }
-
 }
-
-
